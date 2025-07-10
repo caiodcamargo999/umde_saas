e5ac34826a531421ed27f31cd2247d856d988d42
Carolina Abreu <you@example.com>
2025-07-10 18:26:52 +0100

    Atualização do projeto Umde Saas

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-// Se houver erro de importação no Netlify, converta este arquivo para next.config.js
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-export default nextConfig;
